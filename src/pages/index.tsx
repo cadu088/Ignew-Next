37811f709d988871e5f1e60a4b2a0e1459ad4cac
Carlos Eduardo Rodrigues da Silva <carlos.rodrigues@zema.com>
2021-11-30 17:07:22 -0300

    Config chamada api, SSH e SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import  Head  from 'next/head';
+import { GetServerSideProps } from 'next';
 import React from 'react';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+	product: {
+		priceId: string,
+		amount: number, 
+	}
+}
+
+export default function Home({product}: HomeProps) {
   return (
 		<>
 		<Head><title>Home | Ig.News</title></Head>
@@ -12,12 +21,29 @@ export default function Home() {
 				<span>👏 Hey, welcome</span>
 				<h1>New about the <span>React</span> world.</h1>
 				<p>Get access to all the publications <br/>
-				<span>for $9.90 month</span>
+				<span>for {product.amount} month</span>
 				</p>
-				<SubscribeButton/>
+				<SubscribeButton priceId={product.priceId}/>
 			</section>
 			<img src="/sorces/avatar.svg" alt="Girl coding"/>
 		</main>
 		</>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const price = await stripe.prices.retrieve('price_1K1ZEDBFLcEAfkXOP9zz7sPZ')
+
+	const product = {
+		priceId: price.id,
+		amount: new Intl.NumberFormat('es-US', {
+			style: 'currency',
+			currency: 'USD',
+		}).format(price.unit_amount / 100), 
+	}
+	return { 
+		props:{
+			product,
+		}
+	}
 }
